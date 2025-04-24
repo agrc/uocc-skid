30b9d4db05083b45aebdc6bdf149f3ebd1b5859c
Jake Adams <jdadams@utah.gov>
2025-04-24 13:29:46 -0600

    feat: update csvs in media folder

--- tests/test_uoccskid.py
+++ tests/test_uoccskid.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from uocc import main
 
 
@@ -19,6 +21,76 @@ def test_get_secrets_from_local_location(mocker):
 
     assert secrets == {"foo": "bar"}
     assert exists_mock.call_count == 2
+
+
+class TestLocationsExtracting:
+    def test_extract_locations_from_sheet_only_returns_opens(self, mocker):
+        input_dataframe = pd.DataFrame(
+            {
+                "Status": ["Deactivated", "open", "Open"],
+                "ID#": ["UOCC-1234", "UOCC-5678", "UOCC-9101"],
+                "Local Health Department": ["foo", "bar", "baz"],
+                "UOCC Email Address": ["foo", "bar", "baz"],
+                "Corporate Email Address": ["foo", "bar", "baz"],
+                "Corporate Contact Name": ["foo", "bar", "baz"],
+                "UOCC Contact Name": ["foo", "bar", "baz"],
+            }
+        )
+
+        GSheetLoader_mock = mocker.patch("uocc.main.extract.GSheetLoader")
+        GSheetLoader_mock.return_value.load_specific_worksheet_into_dataframe.return_value = input_dataframe
+
+        output_dataframe = main.Skid._extract_locations_from_sheet(mocker.Mock())
+
+        expected_dataframe = pd.DataFrame(
+            {
+                "Status": ["open", "Open"],
+                "ID#": ["UOCC-5678", "UOCC-9101"],
+            },
+            index=[1, 2],
+        )
+
+        pd.testing.assert_frame_equal(expected_dataframe, output_dataframe)
+
+    def test_clean_location_field_names_removes_newlines_spaces_hashes(self, mocker):
+        input_dataframe = pd.DataFrame(
+            {
+                "Status": ["open", "Open"],
+                "Accepts Materials\nDropped Off\nBy Public": ["foo", "bar"],
+                "Facility Name": ["foo", "bar"],
+                "ID#": ["foo", "bar"],
+            }
+        )
+
+        output_dataframe = main.Skid._clean_field_names(mocker.Mock(), input_dataframe)
+
+        expected_dataframe = pd.DataFrame(
+            {
+                "Status": ["open", "Open"],
+                "AcceptsMaterialsDroppedOffByPublic": ["foo", "bar"],
+                "FacilityName": ["foo", "bar"],
+                "ID": ["foo", "bar"],
+            },
+        )
+
+        pd.testing.assert_frame_equal(expected_dataframe, output_dataframe)
+
+    def test_fix_apostrophes_bug_replaces_with_html_entity(self, mocker):
+        input_dataframe = pd.DataFrame(
+            {
+                "FacilityName": ["Autozone", "O'Reilly"],
+            }
+        )
+
+        output_dataframe = main.Skid._fix_apostrophes_bug(mocker.Mock(), input_dataframe)
+
+        expected_dataframe = pd.DataFrame(
+            {
+                "FacilityName": ["Autozone", "OReilly"],
+            },
+        )
+
+        pd.testing.assert_frame_equal(expected_dataframe, output_dataframe)
 
 
 class TestQuestionsRenaming:
