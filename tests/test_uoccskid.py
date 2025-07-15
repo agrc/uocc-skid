26f363629d5ec3bed4b2c645d064529c3d8820eb
Jake Adams <jdadams@utah.gov>
2025-07-15 13:24:25 -0600

    tests: refactor for Skid class

--- tests/test_uoccskid.py
+++ tests/test_uoccskid.py
@@ -6,10 +6,11 @@ from uocc import main
 def test_get_secrets_from_gcp_location(mocker):
     mocker.patch("pathlib.Path.exists", return_value=True)
     mocker.patch("pathlib.Path.read_text", return_value='{"foo":"bar"}')
+    mocker.patch("google.auth.default", return_value=("sa", 42))
 
-    secrets = main._get_secrets()
+    secrets = main.Skid._get_secrets()
 
-    assert secrets == {"foo": "bar"}
+    assert secrets == {"foo": "bar", "SERVICE_ACCOUNT_JSON": "sa"}
 
 
 def test_get_secrets_from_local_location(mocker):
@@ -17,7 +18,7 @@ def test_get_secrets_from_local_location(mocker):
     mocker.patch("pathlib.Path.exists", new=exists_mock)
     mocker.patch("pathlib.Path.read_text", return_value='{"foo":"bar"}')
 
-    secrets = main._get_secrets()
+    secrets = main.Skid._get_secrets()
 
     assert secrets == {"foo": "bar"}
     assert exists_mock.call_count == 2
